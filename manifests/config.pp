f87e45480332e0bf64c2c0c32476e03409da8fa9
Samuli Seppänen <samuli@openvpn.net>
2014-12-11 16:26:05 +0000

    Added support for managing webapp directory permissions
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,6 +9,38 @@ class pwm::config
 
 ) inherits pwm::params
 {
+
+    include tomcat::params
+
+    # Allow pwm to write to the webapp directories it needs
+    file { 'pwm-WEB-INF':
+        ensure => directory,
+        name => "${::tomcat::params::autodeploy_dir}/pwm/WEB-INF",
+        owner => "${::tomcat::params::user}",
+        group => "${::tomcat::params::group}",
+        mode => 755,
+        require => Class['pwm::install'],
+    }
+
+    file { 'pwm-logs':
+        ensure => directory,
+        name => "${::tomcat::params::autodeploy_dir}/pwm/WEB-INF/logs",
+        owner => "${::tomcat::params::user}",
+        group => "${::tomcat::params::group}",
+        mode => 755,
+        require => Class['pwm::install'],
+    }
+
+    file { 'pwm-LocalDB':
+        ensure => directory,
+        name => "${::tomcat::params::autodeploy_dir}/pwm/WEB-INF/LocalDB",
+        owner => "${::tomcat::params::user}",
+        group => "${::tomcat::params::group}",
+        mode => 755,
+        require => Class['pwm::install'],
+    }
+
+    # Directory service-specific configuration
     case $dirsrv_type {
         '389ds': {
             include pwm::config::dirsrv
