2c09968c7aee38df3e3c5e3a9e04e679784552e5
Samuli Seppänen <samuli@openvpn.net>
2018-02-28 15:13:03 +0200

    Vagrant: fix iptables rule
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vagrant/dirsrv.pp
+++ vagrant/dirsrv.pp
@@ -16,9 +16,9 @@ class { '::dirsrv':
     server_admin_pwd             => 'vagrant',
     config_directory_admin_pwd   => 'vagrant',
     admin_bind_ip                => '0.0.0.0',
-    dirsrv_allow_ipv4_address    => '0.0.0.0',
+    dirsrv_allow_ipv4_address    => '0.0.0.0/0',
     dirsrv_allow_ipv6_address    => '::1',
-    admin_srv_allow_ipv4_address => '0.0.0.0',
+    admin_srv_allow_ipv4_address => '0.0.0.0/0',
     admin_srv_allow_ipv6_address => '::1',
     allow_anonymous_access       => 'on',
 }
