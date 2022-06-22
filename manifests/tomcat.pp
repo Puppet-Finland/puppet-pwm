866158032bec8caa512834b20591c41ed14631c6
Samuli Seppänen <samuli@openvpn.net>
2022-06-22 15:59:32 +0300

    Manage Tomcat service
    Also fix a typo in package name
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -7,18 +7,24 @@ class pwm::tomcat
   String $admin_user,
   String $admin_user_password
 ){
-  package { ['tomcat', 'tomcat9-admin']:
+  package { ['tomcat9', 'tomcat9-admin']:
     ensure => 'present',
   }
 
-  $tomcat_users_xml_params = { 'admin_user'          => $admin_user,
-                               'admin_user_password' => $admin_user_password, }
+  service { 'tomcat9':
+    ensure => 'running',
+    enable => true,
+  }
+
+  $tomcat_users_xml_params = {'admin_user'          => $admin_user,
+                              'admin_user_password' => $admin_user_password, }
 
   file { '/etc/tomcat9/tomcat-users.xml':
     ensure  => 'present',
     owner   => 'root',
     group   => 'root',
     mode    => '0700',
-    content => epp('pwm/tomcat-users.xml.epp', $tomcat_users_xml_params)
+    content => epp('pwm/tomcat-users.xml.epp', $tomcat_users_xml_params),
+    notify  => Service['tomcat9'],
   }
 }
